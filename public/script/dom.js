2fdd991489e0f95b8584f1d8716dfab2b002692d
AhmedSafi97 <ahmadhsafi1997@gmail.com>
2020-02-19 14:35:02 +0200

    removed unnecessary function relates #23

--- public/script/dom.js
+++ public/script/dom.js
@@ -11,17 +11,13 @@ const deleteNodeChilds = (node) => {
   while (node.firstChild) node.removeChild(node.firstChild);
 };
 
-(() => {
-  deleteNodeChilds(section);
-  fetchAnimeGiphy().then((response) => response.json())
-    .then((animesGiphy) => {
-      if (animesGiphy.data.length === 0) {
-        throw new TypeError('No Result');
-      }
-      animesGiphy.data.forEach((element) => {
-        addImg(element.images.original.url, section, '200px');
-      });
-    })
-    // eslint-disable-next-line no-console
-    .catch((error) => console.log(error));
-})();
+fetchAnimeGiphy().then((response) => response.json())
+  .then((animesGiphy) => {
+    if (animesGiphy.data.length === 0) {
+      throw new TypeError('No Result');
+    }
+    animesGiphy.data.forEach((element) => {
+      addImg(element.images.original.url, section, '200px');
+    });
+  })
+  .catch((error) => console.log(error));
