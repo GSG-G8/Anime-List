f86bb02fb71e7d391481918eb8ccc4618cad6995
Malek.Zd <muhammadamin@pop-os.localdomain>
2020-02-19 10:26:27 +0200

    Post Request from the front-end Relates #7

---
+++ public/script/fetch.js
@@ -0,0 +1,9 @@
+const fetchAnimeData = (node) => {
+  fetch('/anime', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ anime: node.value }),
+  });
+};
